c683781bc32b04af9960a5397f34277eec92abf9
Minh Vu Trinh <mx-vu@mx-pc>
2020-12-05 15:05:12 +0700

    overhaul of code, better performace

--- mute.sh
+++ mute.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
-previousNumber=65
-com=unmute
-case $com in 
-	"mute")
-   		volume="$(amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2}')"
-   		sed -i "2s/.*/previousNumber=${volume%?}/" ${HOME}/Scripts/mute.sh
-   		sed -i "3s/.*/com=unmute/" ${HOME}/Scripts/mute.sh
-   		amixer sset Master 0
-		notify-send "muted" -t 1000
-	;;
-	"unmute")
-   		amixer sset Master $previousNumber%
-   		sed -i "3s/.*/com=mute/" ${HOME}/Scripts/mute.sh
-		notify-send "unmuted" -t 1000
-   	;;
-esac
+pactl set-sink-mute alsa_output.pci-0000_00_1f.3.analog-stereo toggle
+com=mute
+   case $com in
+      "mute")
+         notify-send "muted" -t 1000
+         sed -i "3s/.*/com=unmute/" ${HOME}/Scripts/mute.sh
+      ;;
+      "unmute")
+         notify-send "unmuted" -t 1000
+         sed -i "3s/.*/com=mute/" ${HOME}/Scripts/mute.sh
+      ;;
+   esac
+
+#previousNumber=60
+#com=mute
+#case $com in 
+#	"mute")
+#   		volume="$(amixer sget Master | awk -F"[][]" '/Left:/ { print $2 }')"
+# 		   sed -i "2s/.*/previousNumber=${volume%?}/" ${HOME}/Scripts/mute.sh
+#   		sed -i "3s/.*/com=unmute/" ${HOME}/Scripts/mute.sh
+#   		amixer sset Master 0
+#		   notify-send "muted" -t 1000
+#	;;
+#	"unmute")
+#  		amixer sset Master $previousNumber%
+#   		sed -i "3s/.*/com=mute/" ${HOME}/Scripts/mute.sh
+#		   notify-send "unmuted" -t 1000
+#   ;;
+#esac
