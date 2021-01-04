a1b1fad95d1f70e7268280dc25a883ced0a5aa10
TrinhMinhVu <vuminhtrinh2804@gmail.com>
2021-01-04 20:02:37 +0700

    fig bugs

--- add-desktop.sh
+++ add-desktop.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 name="$(echo $1 | cut -d "." -f 1 | cut -d "/" -f 2)"
-echo $name
 
 cp ${HOME}/Scripts/notes.desktop ${HOME}/.local/share/applications/$name.desktop
 sed -i "4i Exec=$(pwd)/$1" ${HOME}/.local/share/applications/$name.desktop
--- youtube.sh
+++ youtube.sh
@@ -15,13 +15,13 @@ echo "$query"
 
 # YT_API_KEY location
 YT_API_KEY="$( cat "${HOME}"/.local/api_keys/YT_API_KEY )"
-urlstring="https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=20&key=${YT_API_KEY}"
+urlstring="https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=30&key=${YT_API_KEY}"
 
 mpv "https://$( curl -s "${urlstring}" \
-	| jq -r '.items[] | "\(.snippet.channelTitle) => \(.snippet.title) => youtu.be/\(.id.videoId)"' \
+	| jq -r '.items[] | "\(.snippet.title); from \(.snippet.channelTitle); Id=youtu.be/\(.id.videoId)"' \
 	| rofi -dmenu -i -p 'Select Video -' -theme "~/.cache/wal/colors-rofi-dark.rasi" \
 	| awk '{print $NF}' \
-)"
+)" &
 else 
 	exit 0
 fi
