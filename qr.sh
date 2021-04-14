707c49a1824a0534f6f16b95a3d78ffbb2e6ec15
TrinhMinhVu <vuminhtrinh2804@gmail.com>
2021-04-14 16:05:45 +0700

    Script to create qr code using rofi and qrencoder

---
+++ qr.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -d "$HOME"/Pictures/qrs ];then 
+name=$(date +%dt%mn%Y-%T)
+rofi -dmenu -l 0 -p "Text to encode?" | qrencode -s 3 -l L -o "$HOME/Pictures/qrs/$name.png" && feh -d -g 300x300 -Z --force-aliasing "$HOME"/Pictures/qrs/"$name.png"
+else 
+mkdir "$HOME"/Pictures/qrs
+name=$(date +%dt%mn%Y-%T)
+rofi -dmenu -l 0 -p "Text to encode?" | qrencode -s 3 -l L -o "$HOME/Pictures/qrs/$name.png" && feh -d -g 300x300 -Z --force-aliasing "$HOME"/Pictures/qrs/"$name.png"
+fi
+
